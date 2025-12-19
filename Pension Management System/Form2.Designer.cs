007bb5a08664dd80b84474166b6b75e09ebce229
tamjidniloy <tamjidniloy22@gmail.com>
2025-12-19 22:22:54 +0600

    updated

--- Pension Management System/Form2.Designer.cs
+++ Pension Management System/Form2.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -41,15 +42,24 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "XYZ";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(77, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 100);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form2";
             this.Text = "Form2";
-//            this.Load += new System.EventHandler(this.Form2_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -58,5 +68,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }

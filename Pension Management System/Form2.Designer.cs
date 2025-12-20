a546509fde1be99ef5280704c61a9ba43d4d5b08
tamjidniloy <tamjidniloy22@gmail.com>
2025-12-20 12:30:17 +0600

    updated

--- Pension Management System/Form2.Designer.cs
+++ Pension Management System/Form2.Designer.cs
@@ -44,7 +44,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(77, 166);
+            this.button1.Location = new System.Drawing.Point(86, 100);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(161, 100);
             this.button1.TabIndex = 1;
